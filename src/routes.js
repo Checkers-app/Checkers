4be4fd9b2b6213787952b0e8b8d746c2c10538b8
Nicholas D'Amato <Nicholasvdamato@gmail.com>
2021-06-22 13:01:04 -0600

    finished the skeleton of the page

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,17 @@
 import React from "react";
-import {Switch, Route} from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
 
 import FrontPage from "./components/FrontPage.js";
 import landingpage from "./components/LandingPage.js";
+import ProfilePage from './components/ProfilePage'
 
 
 
 export default (
     <Switch>
-        <Route component = {FrontPage} exact path = "/" />
-        <Route component = {landingpage} path = "/landingpage" />
+        <Route component={FrontPage} exact path="/" />
+        <Route component={landingpage} path="/landingpage" />
+        <Route component={ProfilePage} path="/profile" />
     </Switch>
 )
