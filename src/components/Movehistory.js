6b41a5ee9af601d74c82b02ff8f2f37d0a069736
adpaul3676 <adpaul3676@gmail.com>
2021-06-23 16:00:17 -0600

    styled the turn box

--- src/components/Movehistory.js
+++ src/components/Movehistory.js
@@ -1,11 +1,26 @@
+import '../css/checkerboard.css'
+import React, { useEffect, useRef } from 'react'
+
 const Movehistory = (props) => {
-    return (
-        <div>
-            {props.moves.map(e => {
-                return <h1> {e} </h1>
-            })}
-        </div>
-    )
+
+  const messagesEndRef = useRef(null)
+
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" })
+  }
+
+  useEffect(() => {
+    scrollToBottom()
+  }, [props]);
+
+  return (
+    <div>
+      {props.moves.map(e => {
+        return <h1 className='indTurnText'> {e} </h1>
+      })}
+      <div ref={messagesEndRef} />
+    </div>
+  )
 }
 
 export default Movehistory
