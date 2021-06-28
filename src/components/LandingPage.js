40c42c3a4480a9cdb60cb9e8d78772e4321448a7
adpaul3676 <adpaul3676@gmail.com>
2021-06-28 14:43:19 -0400

    minor styling changes and implementation of routes

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,8 +1,9 @@
 import { Link } from 'react-router-dom';
+import '../css/landingpage.css';
 import Header from "../components/sharedComps/Header.js";
-
 import React, { useContext } from 'react';
-import { UserContext } from '../context/UserContext.js'
+import { UserContext } from '../context/UserContext.js';
+import axios from 'axios';
 
 
 function LandingPage() {
@@ -11,12 +12,33 @@ function LandingPage() {
 
   // console.log(user);
 
+  const logout = () => {
+    axios.get("/auth/logout")
+      .then(res => {
+        console.log(res)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
   return (
     <div className="rooms">
-      <Header />
-      <Link to="/game">Online kinda</Link>
-      <Link to="/1pGame">Pass and Play</Link>
-    </div>
+      <Header className='header' />
+      <section className="bottomContainer">
+        <section className='gameLinks'>
+          <p>Create a Game!</p>
+          <section className="linkContainer">
+            <Link to="/game">Online kinda</Link>
+            <Link to="/1pGame">Pass and Play</Link>
+          </section>
+        </section>
+        <section className='utilityLinks'>
+          <Link to="/ProfilePage" >Profile</Link>
+          <Link to="/" onClick={() => { logout() }}>Logout</Link>
+        </section>
+      </section>
+    </div >
   );
 }
 export default LandingPage;
